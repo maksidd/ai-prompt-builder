5bfb56109b6911d1a511046d5a0b6c0724cae3b3
maksidd <mail.peshkov@gmail.com>
2025-02-23 09:50:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,14 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001', 
+        changeOrigin: true,              
+      },
+    },
+  },
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
